74d6b3a7832c329f565cc07eb90182c5e7f99c41
mihinduranasinghe <mihindu.21523@gmail.com>
2021-01-05 03:40:08 +0530

    WSO2 APIM Cloud CICD newer version beta test

---
+++ README.md
@@ -0,0 +1,81 @@
+# WSO2 APIManager Cloud CICD Action
+
+This action allows WSO2 API Manager Cloud users to automate CICD
+
+## Author
+
+<b>Mihindu Ranasinghe</b> <br/>
+<i>Trainee Software Engineer - WSO2 </i><br/>
+<i>API Manager Cloud Team</i>
+
+## How this works
+
+In this version of APIMCloud CICD Action, you can automate creating an API project with given API definition and publish it in the development tenant. Then it can be tested with a postman collections.
+If all checks passed, API project can be exported from the development tenant to the production tenant.
+
+## Pre requesits
+
+1. Create a github repository
+2. Create a folder named ".github" in the root of the repository
+3. Create a folder named "workflows" inside the ".github" folder.
+4. Create .yml file with any name , inside the "workflow" folder and you need to code as following example in that yml file
+
+5. Replace the example input values with your details (Project name & Version)
+
+Values for the "passwordTargettedTenant" must be stored in github repo secret and use them as ${{secrets.SECRETPASSWORD}} both places.
+
+6. Push it into the main branch
+7. It will initialize a project with standard structure by giving only the project name and the version.
+8. Then you can replace the swagger and api.yml with your files , add mediation sequences , docs and images in relavent standard folders.
+
+9. Set the github action triggering condition to trigger your actions and import projects into tenants when merging the code into relevant branch dedicated to tenant.
+
+10. Go to the action section in the repo to see the CICD pipeline working
+
+## Inputs
+
+### `usernameTargettedTenant`
+
+**Required** Dev tenant username as username@organization.com@DevtenantName
+
+### `passwordTargettedTenant`
+
+**Required** Dev tenant user's password should be stored in github repo secrets and use it here like ${{secrets.PASSWORD}}
+
+### `APIProjectName`
+
+**Required** Give a name for your API Project
+
+### `APIVersion`
+
+**Required** API Version
+
+### `PostmanCollectionTestFile`
+
+**Not Compulsory** Here you can give a postman collection file to test the API before publishing into production tenant
+
+## Example usage
+
+```yaml
+name: WSO2 APIManager Cloud CICD
+on:
+  push:
+    branches: [main, prod]
+
+jobs:
+  apim-cloud-cicd:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Cloning repo into VM
+        uses: actions/checkout@v2.3.4
+
+      - name: APIM Cloud CICD Public Action
+        if: github.event_name == 'push' && github.ref == 'refs/heads/main'
+        uses: mihinduranasinghe/WSO2-APIManager-Cloud-CICD@v1.2.0
+        with:
+          usernameTargettedTenant: "mihindu@wso2.com@development"
+          passwordTargettedTenant: ${{secrets.DEV_TENANT_PASSWORD}}
+          APIProjectName: "SampleStore"
+          APIVersion: "1.0.0"
+          #PostmanCollectionTestFile: "sample_store.postman_collection.json"
+```
---
+++ script.sh
@@ -0,0 +1,80 @@
+#!/bin/sh -l
+
+    # $1 - usernameTargettedTenant
+    # $2 - passwordTargettedTenant
+    # $3 - APIProjectName
+    # $4 - APIVersion
+    # $5 - PostmanCollectionTestFile
+
+
+echo "::group::WSO2 APIMCLI Version"
+    apimcli version
+echo "::end-group"
+
+echo "::group::WSO2 APIMCLI Help"
+    apimcli --help
+echo "::end-group"
+
+echo "::group::WSO2 APIMCloud Tenants"
+    echo Targetted Tenant  - $1
+echo "::end-group"
+
+echo "::group::Add environment wso2apicloud"
+apimcli add-env -n wso2apicloud \
+                      --registration https://gateway.api.cloud.wso2.com/client-registration/register \
+                      --apim https://gateway.api.cloud.wso2.com/pulisher \
+                      --token https://gateway.api.cloud.wso2.com/token \
+                      --import-export https://gateway.api.cloud.wso2.com/api-import-export \
+                      --admin https://gateway.api.cloud.wso2.com/api/am/admin/ \
+                      --api_list https://gateway.api.cloud.wso2.com/api/am/publisher/apis \
+                      --app_list https://gateway.api.cloud.wso2.com/api/am/store/applications
+
+apimcli list envs          
+echo "::end-group"
+
+echo "::group::Init API iproject with given API definition"
+# apictl import-api -f $API_DIR -e $DEV_ENV -k --preserve-provider --update --verbose
+# apimcli init SampleStore --oas petstore.json --definition api_template.yaml
+
+apimcli init ./$3/$4
+
+# apimcli init ./$3/$6 --oas $
+# apimcli init -f ./$3/$6 --oas $ --definition $
+echo "::end-group"
+
+echo "::group::Push API project into the GIT repo from VM"
+git config --global user.email "my-bot@bot.com"
+git config --global user.name "my-bot"
+#This shell script will find all empty directories and sub-directories in a project folder and creates a .gitkeep file, so that the empty directory 
+#can be added to the git index. 
+# find * -type d -empty -exec touch {}/.gitkeep \;
+# find . -type d ! -path "*.git*" -empty -exec touch '{}'/.gitkeep \;
+find * -type d -empty -exec touch '{}'/.gitkeep \;
+
+git add . 
+git commit -m "API project initialized"
+git push
+echo "::end-group"
+
+apimcli login wso2apicloud -u $1 -p $2 -k
+echo "::group::Import API project to targetted Tenant"
+apimcli import-api -f ./$3/$4 -e wso2apicloud --preserve-provider=false --update --verbose -k
+# apimcli logout wso2apicloud 
+echo "::end-group"
+
+echo "::group::List APIS in targetted Tenant"
+# apimcli list apis -e <environment> -k
+# apimcli list apis --environment <environment> --insec# echo "::set-env name=HELLO::hello"ure
+apimcli list apis -e wso2apicloud -k
+echo "::end-group"
+
+echo "::group::Testing With Postman Collection"
+newman run $5 --insecure
+echo "::end-group"
+
+# echo "::group::Export API from current Tenant"
+# # apimcli export-api -n <API-name> -v <version> -r <provider> -e <environment> -u <username> -p <password> -k
+# # apimcli export-api --name <API-name> --version <version> --provider <provider> --environment <environment> --username <username> --password <password> --insecure
+# # apimcli export-api -n TeamMasterAPI -v v1.0.0 -r mihindu@wso2.com@development -e wso2apicloud -k
+# echo "::end-group"
+apimcli logout wso2apicloud 
